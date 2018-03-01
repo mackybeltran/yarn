5607d99563c22692ed7fc4e57bedb40f1c498c52
torshore <toryshoreman@gmail.com>
2018-03-01 10:52:46 -0500

    Initial setup

---
+++ app/components/App.jsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+
+import './App.scss';
+
+class App extends PureComponent {
+    render() {
+        return <div className="app">
+            Hieeeee
+        </div>;
+    }
+}
+
+export default App;
